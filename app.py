1214b94ac60a43c5a6a56ba2a6eedadb062f5de1
Denys Halenok <denys.halenok@gmail.com>
2019-06-20 10:57:44 +0300

    Use SVG instead of PNG; add style option

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import os
 import io
 import falcon
-import msgpack
 import requests
 
-def getBadge(name):
-    image_path = os.path.join(os.getcwd(), f"img/{name}.png")
-    stream = io.open(image_path, 'rb')
+ALLOWED_STYLES = ("flat-square", "plastic")
+
+
+def get_badge(name: str, style: str = None):
+    if style and style in ALLOWED_STYLES:
+        image_path = os.path.join(os.getcwd(), f"img/{name}-{style}.svg")
+    else:
+        image_path = os.path.join(os.getcwd(), f"img/{name}.svg")
+
+    stream = io.open(image_path, "rb")
     content_length = os.path.getsize(image_path)
 
     return stream, content_length
 
 
-class HerokuBadge():
+class HerokuBadge:
     def on_get(self, req, resp):
         if not req.params:
             resp.content_type = "text/html"
@@ -25,20 +31,23 @@ class HerokuBadge():
         if not app:
             resp.status = falcon.HTTP_501
             return
+        style = req.params.get("style")
 
         url = f"https://{app}.herokuapp.com/"
         r = requests.get(url)
-        
+
         resp.cache_control = ("max-age=0", "no-cache", "no-store", "must-revalidate")
         resp.append_header("pragma", "no-cache")
         resp.append_header("expires", 0)
-        resp.content_type = "image/png"
+        resp.content_type = "image/svg+xml"
 
         if r.status_code == 200:
-            resp.stream, resp.content_length = getBadge("deployed")
+            resp.stream, resp.content_length = get_badge(name="deployed", style=style)
+        elif r.status_code == 404:
+            resp.stream, resp.content_length = get_badge(name="not_found", style=style)
         else:
-            resp.stream, resp.content_length = getBadge("failed")
+            resp.stream, resp.content_length = get_badge(name="failed", style=style)
 
 
 application = falcon.API()
-application.add_route('/', HerokuBadge())
+application.add_route("/", HerokuBadge())
